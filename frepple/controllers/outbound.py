266b149a30018f5c86dd4a9c96025528d00508fb
hichamlahlou <hlahlou@frepple.com>
2026-06-08 21:53:30 +0200

    filter routes of reordering rules

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -3210,6 +3210,7 @@ class exporter(object):
             # frepple >= 9.0 has native support for buffers with a min and max level
             for i in self.generator.getData(
                 "stock.warehouse.orderpoint",
+                search=[("route_id.name", "=", "Comprar")],
                 fields=[
                     "warehouse_id",
                     "product_id",
