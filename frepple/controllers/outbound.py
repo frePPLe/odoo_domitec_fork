a656aa66ef67e453477d5fcbf05f08bcac9fdd8f
hichamlahlou <hlahlou@frepple.com>
2026-08-04 17:41:13 +0200

    remove the argentinian time zone

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -26,8 +26,8 @@ import json
 import logging
 import pytz
 from xml.sax.saxutils import quoteattr
-from datetime import datetime, timedelta, date, time, timezone
-from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta
+from pytz import timezone
 
 import odoo
 
@@ -420,21 +420,9 @@ class exporter(object):
         return "P%dDT%dH%dM%sS" % (d.days, hours, minutes, seconds_str)
 
     def formatDateTime(self, d, tmzone=None):
-        # 1. Ensure d is a datetime
-        if isinstance(d, date) and not isinstance(d, datetime):
-            # A pure date: treat as midnight UTC and no conversion
-            dt_midnight = datetime.combine(d, time(0, 0))
-            return dt_midnight.strftime(self.timeformat)
-
-        # 2. Attach UTC tzinfo (now it's aware UTC)
-        d = d.replace(tzinfo=timezone.utc)
-
-        # 3. Convert to target tz
-        tz = ZoneInfo(tmzone or "America/Argentina/Buenos_Aires")
-        d_local = d.astimezone(tz)
-
-        # 4. Format
-        return d_local.strftime(self.timeformat)
+        if not isinstance(d, datetime):
+            d = datetime.fromisoformat(d)
+        return d.astimezone(timezone(tmzone or self.timezone)).strftime(self.timeformat)
 
     def export_users(self):
         users = []
