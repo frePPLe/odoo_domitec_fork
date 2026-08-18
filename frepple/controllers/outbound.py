24388c1cc0763fa5f719815a570c0289fa41665d
hichamlahlou <hlahlou@frepple.com>
2026-08-18 15:12:22 +0200

    another try

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -3173,7 +3173,15 @@ class exporter(object):
                             if wo.is_user_working:
                                 dt = now
                             else:
-                                dt = wo.date_start or i.date_start
+                                dt = (
+                                        wo.date_start
+                                        if wo.date_start
+                                        else (
+                                            wo.date_start
+                                            if wo.date_start
+                                            else i.date_start
+                                        )
+                                    )
                             wo_date = ' start="%s"' % self.formatDateTime(dt)
                     except Exception:
                         wo_date = ""
